4c907e68154d080a309a1e9cd1127e31c72c2893
sofvckinmadguy <segalzakhar1999@mail.ru>
2020-10-09 15:34:55 +0300

    added register UICollectionViewCell from code(not from nib)

--- Sources/UIKitools/Extensions/UICollectionView + Ext.swift
+++ Sources/UIKitools/Extensions/UICollectionView + Ext.swift
@@ -9,58 +9,70 @@
 import UIKit
 
 extension UICollectionReusableView {
-    static var autoReuseIdentifier: String {
-        return NSStringFromClass(self) + "AutogenerateIdentifier"
-    }
+  static var autoReuseIdentifier: String {
+    return NSStringFromClass(self) + "AutogenerateIdentifier"
+  }
 }
 
 public extension UICollectionView {
-    var currentPageNumber: Int {
-        return Int(ceil(self.contentOffset.x / self.frame.size.width))
+  var currentPageNumber: Int {
+    return Int(ceil(self.contentOffset.x / self.frame.size.width))
+  }
+  
+  func dequeue<T: UICollectionReusableView>(cell: T.Type, indexPath: IndexPath) -> T? {
+    return dequeueReusableCell(withReuseIdentifier: T.autoReuseIdentifier, for: indexPath) as? T
+  }
+  
+  func dequeue<T: UICollectionReusableView>(header: T.Type, indexPath: IndexPath) -> T? {
+    return  dequeueReusableSupplementaryView(
+      ofKind: UICollectionView.elementKindSectionHeader,
+      withReuseIdentifier: T.autoReuseIdentifier,
+      for: indexPath)    as? T
+  }
+  
+  func dequeue<T: UICollectionReusableView>(footer: T.Type, indexPath: IndexPath) -> T? {
+    return  dequeueReusableSupplementaryView(
+      ofKind: UICollectionView.elementKindSectionFooter,
+      withReuseIdentifier: T.autoReuseIdentifier,
+      for: indexPath)    as? T
+  }
+  
+  func registerNibCell<T: UICollectionReusableView>(_ cell: T.Type) {
+    register(nibFromClass(cell), forCellWithReuseIdentifier: cell.autoReuseIdentifier)
+  }
+  
+  func registerNibSectionHeader<T: UICollectionReusableView>(_ header: T.Type) {
+    register(nibFromClass(header), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+             withReuseIdentifier: header.autoReuseIdentifier)
+  }
+  
+  func registerNibSectionFooter<T: UICollectionReusableView>(_ footer: T.Type) {
+    register(nibFromClass(footer), forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
+             withReuseIdentifier: footer.autoReuseIdentifier)
+  }
+  
+  func registerCell<T: UICollectionReusableView>(_ cell: T.Type) {
+    register(cell, forCellWithReuseIdentifier: cell.autoReuseIdentifier)
+  }
+  
+  func registerSectionHeader<T: UICollectionReusableView>(_ header: T.Type) {
+    register(header, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: cell.autoReuseIdentifier)
+  }
+  
+  func registerSectionFooter<T: UICollectionReusableView>(_ footer: T.Type) {
+    register(footer, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: cell.autoReuseIdentifier)
+  }
+  
+  // Private
+  
+  fileprivate func nibFromClass(_ type: UICollectionReusableView.Type) -> UINib? {
+    guard let nibName = NSStringFromClass(type).components(separatedBy: ".").last
+    else {
+      return nil
     }
     
-    func dequeue<T: UICollectionReusableView>(cell: T.Type, indexPath: IndexPath) -> T? {
-        return dequeueReusableCell(withReuseIdentifier: T.autoReuseIdentifier, for: indexPath) as? T
-    }
-    
-    func dequeue<T: UICollectionReusableView>(header: T.Type, indexPath: IndexPath) -> T? {
-        return  dequeueReusableSupplementaryView(
-            ofKind: UICollectionView.elementKindSectionHeader,
-            withReuseIdentifier: T.autoReuseIdentifier,
-            for: indexPath)    as? T
-    }
-    
-    func dequeue<T: UICollectionReusableView>(footer: T.Type, indexPath: IndexPath) -> T? {
-        return  dequeueReusableSupplementaryView(
-            ofKind: UICollectionView.elementKindSectionFooter,
-            withReuseIdentifier: T.autoReuseIdentifier,
-            for: indexPath)    as? T
-    }
-    
-    func registerCell<T: UICollectionReusableView>(_ cell: T.Type) {
-        register(nibFromClass(cell), forCellWithReuseIdentifier: cell.autoReuseIdentifier)
-    }
-    
-    func registerSectionHeader<T: UICollectionReusableView>(_ header: T.Type) {
-        register(nibFromClass(header), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                 withReuseIdentifier: header.autoReuseIdentifier)
-    }
-    
-    func registerSectionFooter<T: UICollectionReusableView>(_ footer: T.Type) {
-        register(nibFromClass(footer), forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                 withReuseIdentifier: footer.autoReuseIdentifier)
-    }
-    
-    // Private
-    
-    fileprivate func nibFromClass(_ type: UICollectionReusableView.Type) -> UINib? {
-        guard let nibName = NSStringFromClass(type).components(separatedBy: ".").last
-            else {
-                return nil
-        }
-        
-        return UINib(nibName: nibName, bundle: nil)
-    }
+    return UINib(nibName: nibName, bundle: nil)
+  }
 }
 
 
